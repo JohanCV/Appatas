0caab33cbe6c08f59b8c5780375fe714ebdfab83
JohanCV <nahoj1992@gmail.com>
2019-06-05 13:24:03 -0500

    version 1.0 creacion del proyecto

---
+++ app/src/main/java/jcv/com/appatas/MainActivity.java
@@ -0,0 +1,14 @@
+package jcv.com.appatas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
